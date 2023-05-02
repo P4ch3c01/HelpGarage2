8e3bb26d2eebb9aa15fa91fd5afd96aa08c202ff
Henrique Pacheco <20191GR.TII_I0577@gravatai.ifsul.edu.br>
2023-05-02 08:48:17 -0300

    versão 2 tcc

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3001
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, '/public')))
+app.set('view engine', 'ejs');
+app.get('/index', function (req, res){
+    res.render('index')
+})
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+  app.get('/login', function (req, res){
+    res.render('login')
+})
